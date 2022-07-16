d3ec0a6c2924daac06cbd4f34729534bb9333591
Advay15 <79146419+Advay15@users.noreply.github.com>
2022-07-16 22:05:04 +0530

    Add files via upload

---
+++ b.js
@@ -0,0 +1,48 @@
+img = "";
+status = "";
+objects = [];
+
+function preload() {
+  img = loadImage('b.webp');
+
+}
+
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+  objectDetector.detect(img, gotResult);
+}
+
+function draw() {
+  image(img, 0, 0, 640, 420);
+
+  if (status != "") {
+    console.log(status);
+    for (i = 0; i < objects.length; i++) {
+      document.getElementById("status").innerHTML = "Status : Object Detected";
+
+      fill("#ff1100");
+      percent = floor(objects[i].confidence * 100);
+      text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+      noFill();
+      stroke("#ff1100");
+      rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+  }
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
